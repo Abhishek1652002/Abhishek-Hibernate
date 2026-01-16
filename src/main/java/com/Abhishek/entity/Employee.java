5c39cc6da368590b8c9cb43a46bb0701dc12c367
Abhishek1652002 <abhishekparas77@gmail.com>
2026-01-16 17:40:46 +0530

    update

--- src/main/java/com/Abhishek/entity/Employee.java
+++ src/main/java/com/Abhishek/entity/Employee.java
@@ -1,135 +1,74 @@
 package com.Abhishek.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+
 //POJO CLASS 
 
+@Entity
 public class Employee {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
-	private String name,gender;
+
+	private String name, gender;
 	private int salary;
 	
-	
-	
-	
+	@Transient
+	private String country;
+
 	public Employee() {
 		super();
 	}
-	
-	
-	
-	
-	public Employee( int id ,String name, String gender, int salary) {
+
+	public Employee(String name, String gender, int salary,String country) {
 		super();
-		
-		
-		
-		
-		
-		this.id = id;
 		this.name = name;
 		this.gender = gender;
 		this.salary = salary;
+		this.country = country;
 	}
-
-
-
-
-
-	
-
-
-
-
 
 	public int getId() {
 		return id;
 	}
 
-
-
-
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
-
-
-
 
 	public String getName() {
 		return name;
 	}
 
-
-
-
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
-
-
-
 
 	public String getGender() {
 		return gender;
 	}
 
-
-
-
-
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
-
-
-
-
 
 	public int getSalary() {
 		return salary;
 	}
 
-
-
-
-
 	public void setSalary(int salary) {
 		this.salary = salary;
 	}
-
-
-
-
 
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", name=" + name + ", gender=" + gender + ", salary=" + salary + "]";
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
--- src/main/java/com/Abhishek/main/EmployeeRunner.java
+++ src/main/java/com/Abhishek/main/EmployeeRunner.java
@@ -11,6 +11,7 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
+import com.Abhishek.config.EmpConfiguration;
 import com.Abhishek.entity.Employee;
 
 public class EmployeeRunner {
@@ -19,59 +20,22 @@ public class EmployeeRunner {
 		
 		//Persistent object
 		
-		Employee emp=new Employee(7,"Anshu","male",87000);
+		
+		Employee emp=new Employee("Aditya","Male",98000,"India");
 		
 		
-		//Deprecated
-	//	Configuration cfg=new Configuration().configure("hibernate.cfg.xml");
-	//	SessionFactory sessionFactory=cfg.buildSessionFactory();
-	//	SessionFactory sessionFactory=new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-		
-		//NEW METHOD
-		
-	//	StandardServiceRegistry ssr=new StandardServiceRegistryBuilder().configure().build(); 
-	//	Metadata metadata=new MetadataSources(ssr).getMetadataBuilder().build();
-	//	SessionFactory sessionFactory=metadata.buildSessionFactory();
+		//SessionFactory sessionFactory=EmpConfiguration.getSessionFactory();
 		
 		
-		
-		//NEW METHOD THROUGH METHOD CHAINING
-		SessionFactory sessionFactory=new MetadataSources(new StandardServiceRegistryBuilder().configure().build()).getMetadataBuilder().build().buildSessionFactory();
-		
-		
-		
-		
-		
-		
-		
-		
-		Session session=sessionFactory.openSession();
+		Session session=EmpConfiguration.getSessionFactory().openSession();
 		
 		
 		Transaction tn=session.beginTransaction();
 		
 		
-		//session.persist(emp);
-		//tn.commit();
+		session.persist(emp);
+		tn.commit();
 		
-		
-		
-		//deprecated
-		
-		//Employee employee=session.get(Employee.class, 7);//it gives NULL
-		//System.out.println(employee);
-		
-		
-		Employee employee=session.find(Employee.class, 5);
-		System.out.println(employee);
-		
-		
-		
-		
-		//Employee employee=new Employee();
-		//session.load( employee,7);//object not found exception
-		
-		//System.out.println(employee);
 		
 		
 		
